4c1a88e8ad3bf553b4dc5199c0f04e9a7c7aa0ce
abel <abel@kakaopage.com>
2020-07-24 18:31:31 +0900

    멀티 모듈로 분리

---
+++ module-common/src/test/java/com/rest/api/social/KakaoServiceTest.java
@@ -0,0 +1,32 @@
+package com.rest.api.social;
+
+import com.rest.api.model.social.KakaoProfile;
+import com.rest.api.service.social.KakaoService;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KakaoServiceTest {
+
+    @Autowired
+    private KakaoService kakaoService;
+
+    @Test @Ignore
+    public void whenGetKakaoProfile_thenReturnProfile() {
+
+        String accessToken = "xjsMzpQtIr4w13FIQvL3R7BW7X4yvm1KmzXCTwopyWAAAAFqMxEcwA";
+        // given
+        KakaoProfile profile = kakaoService.getKakaoProfile(accessToken);
+        // then
+        assertNotNull(profile);
+        assertEquals(profile.getId(), Long.valueOf(1066788171));
+    }
+}
